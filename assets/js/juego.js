d90ce34bf55110db1df3b5cf8cf7ee6639e93da5
Andres <castroandresrm@gmail.com>
2021-02-18 21:20:43 -0400

    Agregamos la funcion de obtenerCarta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -3,6 +3,7 @@ let deck         = [];
 const tipos      = ['C','D','H','S'];
 const especiales = ['A','J','Q','K'];
 
+//Crear la Baraja de cartas
 const crearDecks = () =>{
     for (let i = 2; i <= 10; i++) {
         for(let tipo of tipos){
@@ -20,3 +21,15 @@ const crearDecks = () =>{
     return deck;
 }
 crearDecks();
+
+//extraer la carta de la Baraja
+const obtenerCarta = () => {
+    if (deck.length === 0) {
+        throw 'No hay mas Cartas';
+    }
+    const carta = deck.pop();
+    console.log(deck);
+    console.log(carta);
+    return carta;
+} 
+obtenerCarta();
